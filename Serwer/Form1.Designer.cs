ec2d015c40e1abc94544f1560a9dd33d6e824099
Bartosz <bartek.siatkowski@interia.pl>
2022-01-22 00:13:51 +0100

    Dodaj pliki projektów.

---
+++ Serwer/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Serwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.Komunikaty = new System.Windows.Forms.ListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(301, 97);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(143, 67);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Komunikaty
+            // 
+            this.Komunikaty.FormattingEnabled = true;
+            this.Komunikaty.Location = new System.Drawing.Point(70, 195);
+            this.Komunikaty.Name = "Komunikaty";
+            this.Komunikaty.Size = new System.Drawing.Size(636, 225);
+            this.Komunikaty.TabIndex = 1;
+            this.Komunikaty.SelectedIndexChanged += new System.EventHandler(this.Komunikaty_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Komunikaty);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ListBox Komunikaty;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
