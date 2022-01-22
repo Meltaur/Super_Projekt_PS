e3ca961ba4b2208fbb13015321a8ac10a8b782a3
Wojciech Fierek <48804778+Meltaur@users.noreply.github.com>
2022-01-22 03:36:00 +0200

    Add files via upload

---
+++ Klient/Register.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Klient
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxConfirm = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxEMail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.textBoxConfirm);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBoxEMail);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.buttonRegister);
+            this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Controls.Add(this.textBoxUsername);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1019, 538);
+            this.panel1.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(440, 512);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(117, 24);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Go to LOGIN";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(543, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(233, 31);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Confirm password";
+            // 
+            // textBoxConfirm
+            // 
+            this.textBoxConfirm.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 33.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxConfirm.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxConfirm.Location = new System.Drawing.Point(549, 241);
+            this.textBoxConfirm.Multiline = true;
+            this.textBoxConfirm.Name = "textBoxConfirm";
+            this.textBoxConfirm.PasswordChar = '•';
+            this.textBoxConfirm.Size = new System.Drawing.Size(420, 69);
+            this.textBoxConfirm.TabIndex = 9;
+            this.textBoxConfirm.Text = "password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(543, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 31);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "E-mail";
+            // 
+            // textBoxEMail
+            // 
+            this.textBoxEMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 33.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxEMail.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxEMail.Location = new System.Drawing.Point(549, 115);
+            this.textBoxEMail.Multiline = true;
+            this.textBoxEMail.Name = "textBoxEMail";
+            this.textBoxEMail.Size = new System.Drawing.Size(420, 69);
+            this.textBoxEMail.TabIndex = 7;
+            this.textBoxEMail.Text = "email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(389, 465);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(228, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Already have an account?";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(300, 316);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(163, 28);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Show password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(37, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(37, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Username";
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonRegister.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.buttonRegister.Location = new System.Drawing.Point(43, 363);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(926, 69);
+            this.buttonRegister.TabIndex = 3;
+            this.buttonRegister.Text = "REGISTER";
+            this.buttonRegister.UseVisualStyleBackColor = false;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 33.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxPassword.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxPassword.Location = new System.Drawing.Point(43, 241);
+            this.textBoxPassword.Multiline = true;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '•';
+            this.textBoxPassword.Size = new System.Drawing.Size(420, 69);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.Text = "password";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxUsername.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxUsername.Location = new System.Drawing.Point(43, 115);
+            this.textBoxUsername.Multiline = true;
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(420, 69);
+            this.textBoxUsername.TabIndex = 1;
+            this.textBoxUsername.Text = "username";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1022, 66);
+            this.panel2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(981, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 41);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(404, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PG Betting";
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 538);
+            this.Controls.Add(this.panel1);
+            this.Name = "Register";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxConfirm;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxEMail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Klient/Register.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Klient
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void label4_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.ForeColor = Color.White;
+        }
+
+        private void label4_MouseLeave(object sender, EventArgs e)
+        {
+            label4.ForeColor = Color.Black;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBoxPassword.PasswordChar = '\0';
+
+            }
+            else
+                textBoxPassword.PasswordChar = '•';
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            new Login().Show();
+            this.Hide();
+        }
+
+        private void label8_MouseMove(object sender, MouseEventArgs e)
+        {
+            label8.ForeColor = Color.AliceBlue;
+        }
+
+        private void label8_MouseLeave(object sender, EventArgs e)
+        {
+            label8.ForeColor = Color.Black;
+        }
+
+
+
+        private void textBoxUsername_Enter(object sender, EventArgs e)
+        {
+            String username = textBoxUsername.Text;
+            if (username.ToLower().Trim().Equals("username"))
+            {
+                textBoxUsername.Text = "";
+                textBoxUsername.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxUsername_Leave(object sender, EventArgs e)
+        {
+            String username = textBoxUsername.Text;
+            if (username.ToLower().Trim().Equals("username") || username.Trim().Equals(""))
+            {
+                textBoxUsername.Text = "username";
+                textBoxUsername.ForeColor = Color.LightSlateGray;
+            }
+        }
+
+        private void textBoxEMail_Enter(object sender, EventArgs e)
+        {
+            String email = textBoxEMail.Text;
+            if (email.ToLower().Trim().Equals("email"))
+            {
+                textBoxEMail.Text = "";
+                textBoxEMail.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxEMail_Leave(object sender, EventArgs e)
+        {
+            String email = textBoxEMail.Text;
+            if (email.ToLower().Trim().Equals("email") || email.Trim().Equals(""))
+            {
+                textBoxEMail.Text = "email";
+                textBoxEMail.ForeColor = Color.LightSlateGray;
+            }
+        }
+
+        private void textBoxConfirm_Enter(object sender, EventArgs e)
+        {
+            String pass = textBoxConfirm.Text;
+            if (pass.ToLower().Trim().Equals("password"))
+            {
+                textBoxConfirm.Text = "";
+                textBoxConfirm.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxConfirm_Leave(object sender, EventArgs e)
+        {
+            String pass = textBoxConfirm.Text;
+            if (pass.ToLower().Trim().Equals("password") || pass.Trim().Equals(""))
+            {
+                textBoxConfirm.Text = "password";
+                textBoxConfirm.ForeColor = Color.LightSlateGray;
+            }
+        }
+
+        private void textBoxPassword_Enter(object sender, EventArgs e)
+        {
+            String pass = textBoxPassword.Text;
+            if (pass.ToLower().Trim().Equals("password"))
+            {
+                textBoxPassword.Text = "";
+                textBoxPassword.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxPassword_Leave(object sender, EventArgs e)
+        {
+            String pass = textBoxPassword.Text;
+            if (pass.ToLower().Trim().Equals("password") || pass.Trim().Equals(""))
+            {
+                textBoxPassword.Text = "password";
+                textBoxPassword.ForeColor = Color.LightSlateGray;
+            }
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            //new user
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO users (username, password, email) values ('" + textBoxUsername.Text + "', '" + textBoxPassword.Text + "', '" + textBoxEMail.Text + "' )", db.getConnection());
+
+            command.Parameters.Add("@usn", MySqlDbType.VarChar).Value = textBoxUsername.Text;
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = textBoxPassword.Text;
+            command.Parameters.Add("@email", MySqlDbType.VarChar).Value = textBoxEMail.Text;
+
+            db.otworzPolaczenie();
+
+            //zapytanie do bazy danych
+
+            // czy textboxy zawieraja domyslne dane
+            if (!SprawdzTextBoxy())
+            {
+                //czy user sie powtarza w bazie
+                if (SprawdzDuplikaty())
+                {
+                    MessageBox.Show("This username already exists, try a different one!", "Username", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                //czy email sie powtarza w bazie
+                else if (SprawdzDuplikaty2())
+                {
+                    MessageBox.Show("This email is already registered, use a different one!", "Email", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                //czy hasla sie zgadzaja
+                else if (!SprawdzHasla())
+                {
+                    MessageBox.Show("Passwords do not match!", "Passwords", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    if (command.ExecuteNonQuery() == 1)
+                    {
+                        MessageBox.Show("Succesfully created an account", "Account", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failure", "Account", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                //niekomoletne dane
+                MessageBox.Show("Fill the form", "Form incomplete", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            db.zamknijPolaczenie();
+
+        }
+
+        public Boolean SprawdzDuplikaty()
+        {
+            DB db = new DB();
+
+            String username = textBoxUsername.Text;
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE username ='" + username + "'", db.getConnection());
+
+            command.Parameters.Add("@usn", MySqlDbType.VarChar).Value = username;
+
+            adapter.SelectCommand = command;
+
+            adapter.Fill(table);
+
+            //czy taki username istnieje
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean SprawdzDuplikaty2()
+        {
+            DB db = new DB();
+
+            String email = textBoxEMail.Text;
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE email ='" + email + "'", db.getConnection());
+
+            command.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+            adapter.SelectCommand = command;
+
+            adapter.Fill(table);
+
+            //czy taki email istnieje
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean SprawdzTextBoxy()
+        {
+            String uname = textBoxUsername.Text;
+            String pass = textBoxPassword.Text;
+            String email = textBoxEMail.Text;
+            String conf = textBoxConfirm.Text;
+
+            if (uname.Equals("username") || pass.Equals("password") || email.Equals("email") || conf.Equals("password"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean SprawdzHasla()
+        {
+            String pass = textBoxPassword.Text;
+            String conf = textBoxConfirm.Text;
+
+            if (textBoxPassword.Text.Equals(textBoxConfirm.Text))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
